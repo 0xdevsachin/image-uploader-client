4116cd02bd061669efc1cedde997576564cd0ec7
sachin320 <sachinvermavs786@gmail.com>
2022-01-28 15:08:44 +0530

    Image Upload Button

--- src/components/ImageInput/Image.jsx
+++ src/components/ImageInput/Image.jsx
@@ -3,11 +3,19 @@ import "./inputContainer.css";
 import BgImage from "../../assets/InputBg.svg";
 function Image() {
   return (
-    <div className="input-container">
-      <label className="inputBox">
-        <input accept="image/*" type="file" />
-      </label>
-      <img src={BgImage} />
+    <div>
+      <div className="input-container">
+        <label className="inputBox">
+          <input accept="image/*" type="file" />
+        </label>
+        <img src={BgImage} alt="bg" />
+        <span className="drag">Drag & Drop your image here</span>
+      </div>
+      <div>
+        <span className="drag">or</span>
+        <input accept="image/*" id='btn' type="file" />
+        <label htmlFor="btn" className="button">Upload Image</label>
+      </div>
     </div>
   );
 }
